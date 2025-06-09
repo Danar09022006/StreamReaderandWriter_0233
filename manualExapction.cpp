dbe4eb278e56f2e1025cf4b7ab126efcdcfe972c
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-09 21:07:30 +0700

    mengeksekusi blok

--- manualExapction.cpp
+++ manualExapction.cpp
@@ -3,17 +3,16 @@ using namespace std;
 
 int main()
 {
-    
-    
-    
-    
-    
+    try
+    {
+    cout << "Selamat Belajar di prodi TI UMY" << endl;
+    throw("Hallo"); // melemparkan sebuah integer maka
+    cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
     catch (int a)
     {
-        // blok ini akan dieksekusi
-        cout << "Pengecualian akan dieksekusi" << endl;
-
+    // blok ini akan dieksekusi
+    cout << "Pengecualian akan dieksekusi" << endl;
     }
     catch (...)
     {
