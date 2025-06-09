1425499963cd86997da76488cebd52f65224be9e
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-09 20:58:22 +0700

    membuka file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 int main()
 {
-    
-    
-
-    
-    
+    string baris;
+    string Namafile;
+    cout << "Masukkan Nama file : ";
+    cin >> Namafile;
 
     // membuka file dalam mode menulis
-    ofstream outfile;
-    // menunjik ke sebuah nama file
-    outfile.open(Namafile + ".txt", ios::out);
+    
+    
+    
 
-    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    
 
     // unlimited loop untuk menulis
     while (true)
