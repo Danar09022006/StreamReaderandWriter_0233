950884323a160824f49635ef42b0934679bb4782
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-09 21:05:27 +0700

    memasukkan integer

---
+++ manualExapction.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    
+    
+    
+    
+    
+    }
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+
+    }
+    catch (...)
+    {
+        // jika setelah integer maka blok ini akan diexsekusi
+        cout << "Default pengecualian diexsekusi" << endl;
+    }
+
+    return 0;
+}
