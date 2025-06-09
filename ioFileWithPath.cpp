93718dd5a8cf8ceca2098b18e4a019edae6e3c6b
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-09 21:00:21 +0700

    menulis perulangan loop

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -11,23 +11,22 @@ int main()
     cin >> Namafile;
 
     // membuka file dalam mode menulis
+    ofstream outfile;
+// menunjik ke sebuah nama file
+    outfile.open(Namafile + ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
     
-    
-    
-
-    
-
     // unlimited loop untuk menulis
-    while (true)
-    {
-        cout << "- ";
-        // menampilkan setiap karakter dalam satu baris
-        getline(cin, baris);
-        // loop akan berhenti jika anda memasukkan karakter 'q'
-        if (baris == "q")
-            break;
-        // menulis dan memasukkan nilai dari "baris" ke dalam file
-        outfile << baris << endl;
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
