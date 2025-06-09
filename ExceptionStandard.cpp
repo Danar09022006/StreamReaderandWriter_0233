ff2a740db0c12f5a81427e588d1d98da8930c5e2
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-06-09 21:21:23 +0700

    mengeksekusi object exception

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -12,14 +12,14 @@ int main()
     {
         array<int, 3> data = (10, 20, 30);
         // pesan array 
-      
+          cout << data.at(4) << endl;
         // mengisi array elemen ke 2
     }
-    catch (exception &e)
-    { 
-        // menangkap menggunakan object exception
-        cout << e.what() << endl;
-        // akan dieksekusi
+    
+    
+    
+    
+    
     }
     cout << "Baris program yang terakhir" << endl;
     // penanda 2:.. bahwa program berjalan tanpa berhenti
